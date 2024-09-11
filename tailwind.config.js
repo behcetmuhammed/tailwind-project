39b70471f2f99f980e3110cd8a335d436f22dc3b
Behçet <behcetmuhammed>
2024-09-11 14:58:40 +0300

    Week4 - HW-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+        center: true //Her container sınıfı oluşturulduğunda sınıfı ortala.
+    },
+    extend: {
+      fontFamily: { //dışarıdan eklenen font familyler
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif'],
+      },
+      colors:{  //dışarıdan eklenen font renkler
+          'emir-red': '#8C1A45',
+          'emir-melon': '#FFD369',
+          'emir-grey': '#DDDDDD',
+          'emir-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 }
 
+
+
+// extend: {
+//   fontFamily: {
+//     'sans': ['"Proxima Nova"', ...defaultTheme.fontFamily.sans],
+//   },
